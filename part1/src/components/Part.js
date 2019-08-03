444b5977b7db80ed4ef9fe06430deb6c648f3f53
Andrii Tynok <andriytinok@gmail.com>
2019-08-03 10:25:50 +0300

    1.3

--- part1/src/components/Part.js
+++ part1/src/components/Part.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Part(props) {
     return (
         <p>
-            {props.part} {props.exercises}
+            {props.part.name} {props.part.exercises}
         </p>
     );
 }
