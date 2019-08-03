87e4304a540d85a2596347736d611682f11774d5
Andrii Tynok <andriytinok@gmail.com>
2019-08-03 23:12:04 +0300

    1.7

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 function Statistics({good, bad, neutral}) {
+    const all = good + bad + neutral;
     return (
         <div>
             <h2>Statistics</h2>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {(all / 3).toFixed(2)}</p>
+            <p>positive {all ? (good / all * 100).toFixed(2) : 0}%</p>
         </div>
     );
 }
