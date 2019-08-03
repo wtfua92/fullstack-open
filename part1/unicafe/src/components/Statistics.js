bdbaec0fb3b355278034f5d5fbef41f346ea7cab
Andrii Tynok <andriytinok@gmail.com>
2019-08-03 23:27:12 +0300

    1.10

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,19 +1,7 @@
 import React from 'react';
 
-function Statistics({good, bad, neutral}) {
-    const all = good + bad + neutral;
-    const showStatistics = good > 0 || bad > 0 || neutral > 0;
-    const displayStatistics = () => (
-        <div>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {(all / 3).toFixed(2)}</p>
-            <p>positive {all ? (good / all * 100).toFixed(2) : 0}%</p>
-        </div>
-    );
-    return showStatistics ? displayStatistics() : <p>No feedback given</p>;
+function Statistics({text, value}) {
+    return (<div>{text} {value}</div>)
 }
 
 export default Statistics;
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,6 +8,21 @@ const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const all = good + bad + neutral;
+
+    const displayStatistics = () => {
+        if (good === 0 && neutral === 0 && bad === 0) {
+            return <p>No feedback given</p>;
+        }
+        return (<div>
+            <Statistics text="good" value={good}/>
+            <Statistics text="neutral" value={neutral}/>
+            <Statistics text="bad" value={bad}/>
+            <Statistics text="all" value={all}/>
+            <Statistics text="average" value={(all / 3).toFixed(2)}/>
+            <Statistics text="positive" value={(good / all * 100).toFixed(2) + '%'}/>
+        </div>);
+    };
 
     return (
         <div>
@@ -16,10 +31,25 @@ const App = () => {
             <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
             <Button text="bad" onClick={() => setBad(bad + 1)} />
             <h2>Statistics</h2>
-            <Statistics good={good} bad={bad} neutral={neutral} />
+            {displayStatistics()}
         </div>
     )
 };
+
+/*
+    const showStatistics = good > 0 || bad > 0 || neutral > 0;
+    const displayStatistics = () => (
+        <div>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {(all / 3).toFixed(2)}</p>
+            <p>positive {all ? (good / all * 100).toFixed(2) : 0}%</p>
+        </div>
+    );
+    return showStatistics ? displayStatistics() : <p>No feedback given</p>;
+* */
 
 ReactDOM.render(<App />,
     document.getElementById('root')
