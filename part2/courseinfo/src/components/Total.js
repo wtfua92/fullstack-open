8c9d9a38affcdbd339aa4cab47867bd9811f825b
Andrii Tynok <andriytinok@gmail.com>
2019-08-04 13:34:59 +0300

    2.2 2.3

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function Total(props) {
+function Total({parts}) {
+    console.log(parts);
     return (
         <div>
-            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+            <p>Number of exercises {parts.reduce((a, b) => typeof a !== 'object' ? a + b.exercises : a.exercises + b.exercises)}</p>
         </div>
     );
 }
